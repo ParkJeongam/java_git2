cde9a2c1acf6d13d5c244aad97783b644df522b6
DESKTOP-FQ8HFRN\user <csad123@naver.com>
2023-06-19 15:15:02 +0900

    세번째 푸쉬

--- src/java_git2/halo.java
+++ src/java_git2/halo.java
@@ -5,6 +5,7 @@ public class halo {
 	public static void main(String[] args) {
 		System.out.println("밥을 먹으면 졸리다");
 		System.out.println("커피를 마셔도 잠이 깨지 않는다");
+		System.out.println("게임은 재밌다");
 
 	}
 
